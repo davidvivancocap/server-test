f6487e6c2e233a0a5cdeecf2e4c563fb3b88b9be
David Vivanco <david.vivanco-miquis@capgemini.com>
2021-08-03 12:03:30 +0200

    port changed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000
 
 
 app.get('/', function (req, res) {
@@ -8,11 +9,10 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-    console.log(req.body)
-    res.send({ message: 'Hello from test server' });
+    res.send({ message: 'Hello from test server',body:req.body });
 });
 
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("El servidor está inicializado en el puerto 3000");
 });
