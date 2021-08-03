a6fd748806dc1b3325d77e0bb5bbea3e2f9e5258
David Vivanco <david.vivanco-miquis@capgemini.com>
2021-08-03 13:04:55 +0200

    render html+

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path')
 
 const app = express();
 app.use(express.json())
@@ -7,8 +8,7 @@ const PORT = process.env.PORT || 3000
 
 
 app.get('/', function (req, res) {
-    console.log(req.query)
-    res.send('Saludos desde express');
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.post('/', function (req, res) {
