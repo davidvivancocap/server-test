275b3d8bf1e70c5f8e0ba2793cdfa63ebcf00488
David Vivanco <david.vivanco-miquis@capgemini.com>
2021-08-03 12:22:50 +0200

    express .json

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser);
+app.use(express.json())
 const PORT = process.env.PORT || 3000
 
 
