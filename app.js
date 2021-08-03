7ae2fd4800eebb07f408bfa830650675d93ba426
David Vivanco <david.vivanco-miquis@capgemini.com>
2021-08-03 12:27:26 +0200

    console-body

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
+    console.log(req.body)
     res.send({ message: 'Hello from test server', body: req.body || {} });
 });
 
