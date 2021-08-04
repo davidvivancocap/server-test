8e65a8f90f185160789c7decdecec1aa0d499f40
David Vivanco <david.vivanco-miquis@capgemini.com>
2021-08-04 12:37:42 +0200

    cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require('path')
+const cors = require('cors');
 
 const app = express();
 app.use(express.json())
+app.use(cors())
+
 
 const PORT = process.env.PORT || 3000
 
